da266b7033ed00abbc8f2e088d46fa2384f91f89
Kévin <etkevin85@hotmail.fr>
2019-01-28 12:08:13 +0100

    Page intro
    PB : failed to compile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
-  { path: 'abonnement', loadChildren: './abonnement/abonnement.module#AbonnementPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'cgu', loadChildren: './cgu/cgu.module#CguPageModule' },
-  { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
+  { path: '', redirectTo: 'intro', pathMatch: 'full' },
+  { path: 'home', loadChildren: '../pages/home/home.module#HomePageModule' },
+  { path: 'intro', loadChildren: '../pages/intro/intro.module#IntroPageModule' },
+  { path: 'abonnement', loadChildren: '../pages/abonnement/abonnement.module#AbonnementPageModule' },
+  { path: 'login', loadChildren: '../pages/login/login.module#LoginPageModule' },
+  { path: 'cgu', loadChildren: '../pages/cgu/cgu.module#CguPageModule' },
+  { path: 'contact', loadChildren: '../pages/contact/contact.module#ContactPageModule' },
 
-  { path: 'parametre', loadChildren: './parametre/parametre.module#ParametrePageModule' },
-  { path: 'galerie', loadChildren: './galerie/galerie.module#GaleriePageModule' },
+  { path: 'parametre', loadChildren: '../pages/parametre/parametre.module#ParametrePageModule' },
+  { path: 'galerie', loadChildren: '../pages/galerie/galerie.module#GaleriePageModule' },
 
-  { path: 'parametres', loadChildren: './parametres/parametres.module#ParametresPageModule' },
-  { path: 'galerie', loadChildren: './galerie/galerie.module#GaleriePageModule' },
-  { path: 'flyer', loadChildren: './flyer/flyer.module#FlyerPageModule' },
-  { path: 'conseils', loadChildren: './conseils/conseils.module#ConseilsPageModule' },
-  { path: 'accueil', loadChildren: './accueil/accueil.module#AccueilPageModule' },
-  { path: 'profil', loadChildren: './profil/profil.module#ProfilPageModule' },
-  { path: 'nutristats', loadChildren: './nutristats/nutristats.module#NutristatsPageModule' },
+  { path: 'parametres', loadChildren: '../pages/parametres/parametres.module#ParametresPageModule' },
+  { path: 'galerie', loadChildren: '../pages/galerie/galerie.module#GaleriePageModule' },
+  { path: 'flyer', loadChildren: '../pages/flyer/flyer.module#FlyerPageModule' },
+  { path: 'conseils', loadChildren: '../pages/conseils/conseils.module#ConseilsPageModule' },
+  { path: 'accueil', loadChildren: '../pages/accueil/accueil.module#AccueilPageModule' },
+  { path: 'profil', loadChildren: '../pages/profil/profil.module#ProfilPageModule' },
+  { path: 'nutristats', loadChildren: '../pages/nutristats/nutristats.module#NutristatsPageModule' },
 
 ];
 
